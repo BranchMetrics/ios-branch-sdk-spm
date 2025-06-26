3650cef3c6798b48bd9a0522d2b88dd214557b50
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-06-26 01:15:56 -0700

    Testing Binary Distribution

--- Package.swift
+++ Package.swift
@@ -1,41 +1,26 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "BranchSDK",
+    name: "ios-branch-deep-linking-attribution",
     platforms: [
-        .iOS(.v12),
-        .tvOS(.v12),
-    ],
-    products: [
-        .library(
-            name: "BranchSDK",
-            targets: ["BranchSDK"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "BranchSDK",
-            path: "Sources",
-            sources: [
-                "BranchSDK/"
-            ],
-            resources: [
-                .copy("Resources/PrivacyInfo.xcprivacy"),
-            ],
-            publicHeadersPath: "BranchSDK/Public/",
-            cSettings: [
-                .headerSearchPath("BranchSDK/Private"),
-            ],
-            linkerSettings: [
-                .linkedFramework("CoreServices"),
-                .linkedFramework("SystemConfiguration"),
-                .linkedFramework("WebKit", .when(platforms: [.iOS])),
-                .linkedFramework("CoreSpotlight", .when(platforms: [.iOS])),
-                .linkedFramework("AdServices", .when(platforms: [.iOS]))
-            ]
-        ),
-    ]
+            .iOS(.v13),
+            .tvOS(.v13),
+        ],
+        products: [
+            .library(
+                name: "BranchSDK",
+                targets: ["BranchSDKBinary"]
+            ),
+        ],
+        targets: [
+            .binaryTarget(
+                name: "BranchSDKBinary",
+                url: "https://github.com/NidhiDixit09/nidhidixit09.github.io/raw/refs/heads/main/Branch.zip",
+                checksum: "23f2d42a54ed9c14e4bc1ec93c8b28f5c66c68b26988692a1c154efa1710d868"
+            )
+            
+        ]
 )
+
